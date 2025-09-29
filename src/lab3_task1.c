7fbee848bd2ea2715bac95b9bb72ff02c8b39714
Mehmet Efe Gözalan <mehmetefegozalan4@gmail.com>
2025-09-29 08:12:25 +0000

    3rd week practices

--- src/lab3_task1.c
+++ src/lab3_task1.c
@@ -1,6 +1,6 @@
 /*
  * Lab 3, Task 1
- * Student Name, Student ID
+ * MEHMET EFE GÖZALAN, 150ADB111
  *
  * Implement array algorithms:
  *   - find minimum value
@@ -56,15 +56,30 @@ int array_min(int arr[], int size) {
 
 int array_max(int arr[], int size) {
     // TODO: return largest element
-    return 0; // placeholder
+    int my_max = INT_MIN;
+    for (int i=0;i<size;i++) {
+        if (my_max < arr[i]) {
+            my_max = arr[i];
+        }
+    }
+    return my_max; // placeholder
 }
 
 int array_sum(int arr[], int size) {
     // TODO: return sum of elements
-    return 0; // placeholder
+    int array_sum = 0;
+    for (int i=0;i<size;i++){
+        array_sum += arr[i];
+    }
+    return array_sum; // placeholder
 }
 
 float array_avg(int arr[], int size) {
     // TODO: return average as float
-    return 0.0f; // placeholder
+    float array_avg = 0;
+    for (int i=0;i<size;i++){
+        array_avg += arr[i];
+    }
+    array_avg = array_avg/size;
+    return array_avg; // placeholder
 }
